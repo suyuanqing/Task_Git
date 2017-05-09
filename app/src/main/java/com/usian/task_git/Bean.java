1fd7d3361b1c943469f6e23a370f095251b086e2
jing4531288 <2231100609@qq.com>
2017-05-09 14:45:08 +0800

    qqq

---
+++ app/src/main/java/com/usian/task_git/Bean.java
@@ -0,0 +1,35 @@
+package com.usian.task_git;
+
+/**
+ * Created by 丰 on 2017/5/9.
+ */
+
+public class Bean {
+    private String name;
+    private String sex;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
